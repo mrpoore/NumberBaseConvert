19c4c0f5a4ac753fa995d83e06a9706ec978c6a3
rpoore <rpoore@neisd.net>
2015-01-03 15:54:56 -0600

    Initial Check in of Number Conversion

---
+++ NumConHint/src/NumberConversion.java
@@ -0,0 +1,50 @@
+import javax.swing.JOptionPane;
+
+
+public class NumberConversion {
+
+	static String binary = "";
+	static int decimal;
+	
+	public static void main(String[] args) {
+	
+		String strNbr = "";
+		do{strNbr = JOptionPane.showInputDialog("Enter \n"
+				+ " 1 to convert from decimal to binary \n"
+				+ " 2 to convert from binary to decimal ");
+		}while (!(strNbr.charAt(0) == '1' || strNbr.charAt(0) == '2'));
+		
+		if (strNbr.charAt(0) =='1')
+		{
+				binary = JOptionPane.showInputDialog("Enter a binary number");
+				decimal = bintodec(binary);
+				JOptionPane.showMessageDialog(null, binary + " = " + decimal +" in base 10");
+		} else {	
+				decimal = Integer.parseInt(JOptionPane.showInputDialog("Enter a decimal number"));
+				binary = dectobin(decimal);
+				JOptionPane.showMessageDialog(null, decimal + " = " + binary + " in base 2");
+		}
+	}
+	
+	static private int bintodec(String bin)
+	{
+		return Integer.parseInt(bin, 2);
+	}
+	
+	static private String dectobin(int dec)
+	{
+		binary = "";
+		int exp = 0;
+		while (Math.pow(2, exp) <= dec)
+			exp++;
+		System.out.println(exp);
+		
+		for (int i = exp-1; i >= 0 ; i--)
+		{
+			binary = binary + dec / (int)Math.pow(2, i);
+			dec %= Math.pow(2, i); 
+		}
+		System.out.println(binary);
+		return binary;
+	}
+}
